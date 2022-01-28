0b90e6e4bb100da37d584f0ff9bfb08bd2541b5d
Avi <avistudiocity@gmail.com>
2022-01-28 15:52:43 -0800

    initial commit

--- bin/products-cdk.ts
+++ bin/products-cdk.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { ProductsCdkStack } from '../lib/products-cdk-stack';
+import * as cdk from '@aws-cdk/core';
+import { ProductsCdkStack  } from '../lib/products-cdk-stack';
 
 const app = new cdk.App();
 new ProductsCdkStack(app, 'ProductsCdkStack', {
--- lib/products-cdk-stack.ts
+++ lib/products-cdk-stack.ts
@@ -1,16 +1,63 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from '@aws-cdk/core';
+import  * as dynamodb from '@aws-cdk/aws-dynamodb';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as apigateway from '@aws-cdk/aws-apigateway'
+import * as path from 'path'
+//import { aws_s3 as s3 } from 'aws-cdk-lib';
+//import * as lambdaNode from '@aws-cdk/aws-lambda-nodejs';
 
-export class ProductsCdkStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class ProductsCdkStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+  const table = new dynamodb.Table(this, 'products', {
+    partitionKey: {
+      name: 'id', 
+      type: dynamodb.AttributeType.STRING
+    }
+  });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ProductsCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+  const lambdaFunction = new lambda.Function(this, 'productLambda', {
+    runtime: lambda.Runtime.NODEJS_14_X,
+    handler: 'lambda.handler',
+    code: lambda.Code.fromAsset('resources'), 
+    environment: {
+      DYNAMO_TABLE_NAME: table.tableName
+    }
+  })
+
+  table.grantReadWriteData(lambdaFunction);
+
+  const apiGateway = new apigateway.LambdaRestApi(this, 'productAPI', {
+    handler: lambdaFunction,
+    proxy: false
+  });
+
+  const products = apiGateway.root.addResource('products');
+  products.addMethod('POST');
+  
+  const getProduct = products.addResource('{productId}');
+  getProduct.addMethod('GET');
+
+  const searchProducts = products.addResource('search');
+  searchProducts.addMethod('GET');
+
   }
 }
+
+// import { Stack, StackProps } from 'aws-cdk-lib';
+// import { Construct } from 'constructs';
+// // import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+// export class ProductsCdkStack extends Stack {
+//   constructor(scope: Construct, id: string, props?: StackProps) {
+//     super(scope, id, props);
+
+//     // The code that defines your stack goes here
+
+//     // example resource
+//     // const queue = new sqs.Queue(this, 'ProductsCdkQueue', {
+//     //   visibilityTimeout: cdk.Duration.seconds(300)
+//     // });
+//   }
+// }

bf9aae745b4c03d4759af7d6667c45a8f3f95f54
Avi <avistudiocity@gmail.com>
2022-01-30 01:18:40 -0800

    refactored post method

--- lib/products-cdk-stack.ts
+++ lib/products-cdk-stack.ts
@@ -1,77 +1,39 @@
 import * as cdk from '@aws-cdk/core';
-import  * as dynamodb from '@aws-cdk/aws-dynamodb';
+import * as dynamodb from '@aws-cdk/aws-dynamodb';
 import * as lambda from '@aws-cdk/aws-lambda';
 import * as apigateway from '@aws-cdk/aws-apigateway'
-import * as path from 'path'
-//import  * as nodejs from '@aws-cdk/aws-lambda-nodejs';
-import {NodejsFunction} from '@aws-cdk/aws-lambda-nodejs'
-//import { aws_s3 as s3 } from 'aws-cdk-lib';
-//import * as lambdaNode from '@aws-cdk/aws-lambda-nodejs';
 
 export class ProductsCdkStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-  const table = new dynamodb.Table(this, 'products', {
-    partitionKey: {
-      name: 'id', 
-      type: dynamodb.AttributeType.STRING
-    }
-  });
+    const table = new dynamodb.Table(this, 'products', {
+      partitionKey: {
+        name: 'id',
+        type: dynamodb.AttributeType.STRING
+      }
+    });
 
-  const lambdaFunction = new  lambda.Function(this, 'productLambda', {
-    runtime: lambda.Runtime.NODEJS_14_X,
-    handler: 'lambda.handler',
-    code: lambda.Code.fromAsset('resources'), 
-    environment: {
-      DYNAMO_TABLE_NAME: table.tableName
-    }
-  })
+    const lambdaFunction = new lambda.Function(this, 'productLambda', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: 'lambda.handler',
+      code: lambda.Code.fromAsset('resources'),
+      environment: {
+        DYNAMO_TABLE_NAME: table.tableName
+      }
+    });
+    table.grantReadWriteData(lambdaFunction);
 
-  //ts
-// const lambdaFunction = new NodejsFunction(this, 'productLambda', {
-//     runtime: lambda.Runtime.NODEJS_14_X,
-//     handler: 'lambda.handler',
-//     entry: path.join(__dirname,'resources'), 
-//     environment: {
-//       DYNAMO_TABLE_NAME: table.tableName
-//     }
-//   })
+    const apiGateway = new apigateway.LambdaRestApi(this, 'productAPI', {
+      handler: lambdaFunction,
+      proxy: false
+    });
 
-  
-
-  table.grantReadWriteData(lambdaFunction);
-
-  const apiGateway = new apigateway.LambdaRestApi(this, 'productAPI', {
-    handler: lambdaFunction,
-    proxy: false
-  });
-
-  const products = apiGateway.root.addResource('products');
-  products.addMethod('POST');
-  
-  const getProduct = products.addResource('{productId}');
-  getProduct.addMethod('GET');
-
-  const searchProducts = products.addResource('search');
-  searchProducts.addMethod('GET');
-
+    const products = apiGateway.root.addResource('products');
+    products.addMethod('POST');
+    const getProduct = products.addResource('{productId}');
+    getProduct.addMethod('GET');
+    const searchProducts = products.addResource('search');
+    searchProducts.addMethod('GET');
   }
 }
-
-// import { Stack, StackProps } from 'aws-cdk-lib';
-// import { Construct } from 'constructs';
-// // import * as sqs from 'aws-cdk-lib/aws-sqs';
-
-// export class ProductsCdkStack extends Stack {
-//   constructor(scope: Construct, id: string, props?: StackProps) {
-//     super(scope, id, props);
-
-//     // The code that defines your stack goes here
-
-//     // example resource
-//     // const queue = new sqs.Queue(this, 'ProductsCdkQueue', {
-//     //   visibilityTimeout: cdk.Duration.seconds(300)
-//     // });
-//   }
-// }
--- resources/lambda.js
+++ resources/lambda.js
@@ -3,25 +3,8 @@ import * as dynamo from '@aws-sdk/lib-dynamodb';
 import * as dynamoClient from '@aws-sdk/client-dynamodb'
 
 const ddbClient = new dynamoClient.DynamoDBClient({ region: 'us-east-1' });
-// const marshallOptions = {
-//   // Whether to automatically convert empty strings, blobs, and sets to `null`.
-//   convertEmptyValues: false, // false, by default.
-//   // Whether to remove undefined values while marshalling.
-//   removeUndefinedValues: false, // false, by default.
-//   // Whether to convert typeof object to map attribute.
-//   convertClassInstanceToMap: false, // false, by default.
-// };
-
-// const unmarshallOptions = {
-//   // Whether to return numbers as a string instead of converting them to native JavaScript numbers.
-//   wrapNumbers: false, // false, by default.
-// };
-
-// const translateConfig = { marshallOptions, unmarshallOptions };
-
 const ddbDocClient = dynamo.DynamoDBDocumentClient.from(ddbClient, {});
 
-//create responseObject
 function Response(isBase64Encoded, statusCode, headers, multiValueHeaders, body) {
   this.isBase64Encoded = isBase64Encoded;
   this.statusCode = statusCode;
@@ -30,146 +13,119 @@ function Response(isBase64Encoded, statusCode, headers, multiValueHeaders, body)
   this.body = body;
 }
 
-//ts
-// class Response {
-// isBase64Encoded: boolean;
-//  statusCode: number;
-//   headers: object;
-//   multiValueHeaders: object;
-//   body:string;
+export async function handler(event) {
 
-//   constructor(isBase64Encoded: boolean,  statusCode: number, headers: object,   multiValueHeaders: object, body:string){
-//   this.isBase64Encoded = isBase64Encoded;
-//   this.statusCode = statusCode;
-//   this.headers = headers;
-//   this.multiValueHeaders = multiValueHeaders
-//   this.body = body;
-//   }
-// }
-
-export async function handler(event, context, callback) {
-
-  let errorMessage = '';
-  //helper functions
   const invalidName = (name) => {
     return name.length > 40;
   }
-  const negativePrice = (price) => {
+  const invalidPrice = (price) => {
     return price < 0;
   }
-  const blankTags = (tags) => {
+  const invalidTags = (tags) => {
     return tags.includes('');
   }
 
-  try {
-    //handle create products
-    if (event.httpMethod === 'POST' && event.resource === '/products') {
-      const body = JSON.parse(event.body);
-      if (!body) {
-        throw 'Please provide a valid formatted request in the body.'
-      } else {
-        if (invalidName(body.name)) {
-          errorMessage = 'Product Name must not exceed 40 characters.';
-        }
-        if (negativePrice(body.price)) {
-          errorMessage = 'Price must not be negative.';
-        }
-        if (blankTags(body.tags)) {
-          errorMessage = 'Tags must not be blank.';
-        }
-        if (errorMessage) {
-          return new Response(false, 400, {}, {}, errorMessage);
-        } else {
-          const randomId = (Date.now() + Math.random()).toFixed();
-          const params = {
-            Item: {
-              id: randomId,
-              name: body.name,
-              price: body.price,
-              tags: body.tags
-            },
-            TableName: process.env.DYNAMO_TABLE_NAME
-          }
-          const data = await ddbDocClient.send(new dynamo.PutCommand(params));
-          if (data.$metadata.httpStatusCode === 200) {
-            return new Response(false, 201, {}, {}, JSON.stringify(params.Item));
-          } else {
-            throw 'Unknown error inserting item into DynamoDB';
-          }
-        }
-      }
+  if (event.httpMethod === 'POST' && event.resource === '/products') {
+    const body = JSON.parse(event.body);
+    if (invalidName(body.name)) {
+      return new Response(false, 400, {}, {}, 'Product Name must not exceed 40 characters.');
     }
-
-    //handle get product by Id
-    if (event.httpMethod === 'GET' && event.resource === '/products/{productId}') {
-      const productId = event.pathParameters.productId;
-      if (!productId) {
-        throw 'Please specify a Product Id';
-      } else {
-        const params = {
-          Key: {
-            "id": productId
-          },
-          TableName: process.env.DYNAMO_TABLE_NAME
-        }
-        const data = await ddbDocClient.send(new dynamo.GetCommand(params));
-        return new Response(false, 200, {}, {}, JSON.stringify(data.Item));
-      }
+    if (invalidPrice(body.price)) {
+      return new Response(false, 400, {}, {}, 'Price must not be negative.');
     }
-
-    //handle search products by tags
-    if (event.httpMethod === 'GET' && event.resource === '/products/search') {
-      let tags = event.queryStringParameters?.tags;
-      if (!tags) {
-        errorMessage = 'Please specify at least one tag to filter by.'
-      } else {
-        tags = tags.split(',');
-        const valuesObject = {};
-        tags.forEach((t) => { valuesObject[`:${t}`] = t });
-        let filter = ``;
-        for (const tag in valuesObject) {
-          console.log(tag)
-          if (filter) {
-            filter += ` AND `;
-          } filter += `contains(tags, ${tag})`
-        }
-
-        const params = {
-          FilterExpression: filter,
-          ExpressionAttributeValues: valuesObject,
-          TableName: process.env.DYNAMO_TABLE_NAME
-        }
-
-        let response;
-        await ddbDocClient.send(new dynamo.ScanCommand(params))
-          .then((data) => {
-            response = response = {
-              _records: data.Items
-            }
-            console.log(' in then block  DATA::: ' + JSON.stringify(data));
-          })
-          .catch((error) => {
-            console.log('in catch block ::: ' + error);
-            errorMessage = error
-          });
-
-        return new Response(false, 200, {}, {}, JSON.stringify(response))
-
-      }
-
-      if (errorMessage) {
-        return new Response(false, 400, {}, {}, errorMessage);
-      }
+    if (invalidTags(body.tags)) {
+      return new Response(false, 400, {}, {}, 'Tags must not be blank.');
     }
-
-
-
-  } catch (err) {
-    console.log('in GENERAL catch block ' + err);
-    return new Response(false, 400, {}, {}, JSON.stringify(err));
+    const randomId = (Date.now() + Math.random()).toFixed();
+    const params = {
+      Item: {
+        id: randomId,
+        name: body.name,
+        price: body.price,
+        tags: body.tags
+      },
+      TableName: process.env.DYNAMO_TABLE_NAME
+    }
+    let success = false;
+    let error;
+    await ddbDocClient.send(new dynamo.PutCommand(params))
+      .then(() => {
+        success = true;
+      })
+      .catch((err) => {
+        error = err;
+      });
+    if (success) {
+      return new Response(false, 201, {}, {}, JSON.stringify(params.Item));
+    } else {
+      return new Response(false, 500, {}, {}, JSON.stringify(error));
+    }
   }
 
-  return new Response(false, 400, {}, {}, 'Invalid Request. Make sure you are using the correct method and endpoint')
+  //handle get product by Id
+  if (event.httpMethod === 'GET' && event.resource === '/products/{productId}') {
+    const productId = event.pathParameters.productId;
+    if (!productId) {
+      throw 'Please specify a Product Id';
+    } else {
+      const params = {
+        Key: {
+          "id": productId
+        },
+        TableName: process.env.DYNAMO_TABLE_NAME
+      }
+      const data = await ddbDocClient.send(new dynamo.GetCommand(params));
+      return new Response(false, 200, {}, {}, JSON.stringify(data.Item));
+    }
+  }
+
+  //handle search products by tags
+  if (event.httpMethod === 'GET' && event.resource === '/products/search') {
+    let tags = event.queryStringParameters?.tags;
+    let errorMessage;
+    if (!tags) {
+      errorMessage = 'Please specify at least one tag to filter by.'
+    } else {
+      tags = tags.split(',');
+      const valuesObject = {};
+      tags.forEach((t) => { valuesObject[`:${t}`] = t });
+      let filter = ``;
+      for (const tag in valuesObject) {
+        console.log(tag)
+        if (filter) {
+          filter += ` AND `;
+        } filter += `contains(tags, ${tag})`
+      }
+
+      const params = {
+        FilterExpression: filter,
+        ExpressionAttributeValues: valuesObject,
+        TableName: process.env.DYNAMO_TABLE_NAME
+      }
+
+      let response;
+      await ddbDocClient.send(new dynamo.ScanCommand(params))
+        .then((data) => {
+          response = response = {
+            _records: data.Items
+          }
+          console.log(' in then block  DATA::: ' + JSON.stringify(data));
+        })
+        .catch((error) => {
+          console.log('in catch block ::: ' + error);
+          errorMessage = error
+        });
+
+      return new Response(false, 200, {}, {}, JSON.stringify(response))
+
+    }
+
+    if (errorMessage) {
+      return new Response(false, 400, {}, {}, errorMessage);
+    }
+  }
+
+  return new Response(false, 400, {}, {}, 'Invalid Request. Make sure you are using the correct method and endpoint');
 
 }
 
